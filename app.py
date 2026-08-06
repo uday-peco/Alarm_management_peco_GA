fb9c80049f29ce7496db64b4bbfbb62363d889a0
Uday Arora <uday.arora@exeloncorp.com>
2026-08-06 14:22:38 -0400

    Prepare dashboard for Databricks Apps

---
+++ app.py
@@ -0,0 +1,7 @@
+﻿from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# Serve index.html, CSS, JavaScript, and CSV files from the static folder
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
